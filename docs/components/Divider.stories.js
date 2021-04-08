49381cddd3d4703f35be718aef14160690f77d2d
Agung Yuliyanto <agung.yuliyanto@gramedia.digital>
2021-04-08 09:59:14 +0700

    Divider: init divider component

---
+++ docs/components/Divider.stories.js
@@ -0,0 +1,18 @@
+import { } from "./Divider";
+
+export default {
+    title: 'Divider',
+    argTypes: { },
+    parameters: {
+        docs: {
+            page: null
+        }
+    },
+}
+
+const Template = ({ ...args }) => {
+    // return createDiv(args);
+    return '';
+}
+
+export const Divider = Template.bind({});
